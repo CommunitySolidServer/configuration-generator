f9ab266fda19780e8287c2cea87d40f9bba370c3
Joachim Van Herwegen <joachimvh@gmail.com>
2023-03-22 10:09:02 +0100

    Create HTML dir

--- src/build.ts
+++ src/build.ts
@@ -1,10 +1,12 @@
-import { writeFile } from 'fs/promises';
+import { mkdir, writeFile } from 'fs/promises';
 import { join } from 'path';
 import { renderFile } from 'ejs';
 import { CHOICES } from './util/config';
 
 async function generateHtml(): Promise<void> {
   const html = await renderFile(join(__dirname, '../templates/index.html.ejs'), { choices: CHOICES });
-  await writeFile(join(__dirname, '../html/index.html'), html, 'utf-8');
+  const htmlDir = join(__dirname, '../html/');
+  await mkdir(htmlDir, { recursive: true });
+  await writeFile(join(htmlDir, 'index.html'), html, 'utf-8');
 }
 generateHtml();
