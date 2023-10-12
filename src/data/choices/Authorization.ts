507707b01241069a9a1380f6c1fd17c3f6dcfbff
Joachim Van Herwegen <joachimvh@gmail.com>
2023-10-12 14:33:01 +0200

    Update authorization warning

--- src/data/choices/Authorization.ts
+++ src/data/choices/Authorization.ts
@@ -10,8 +10,9 @@ export const AUTHORIZATION = {
   </p><p class="text-danger"><i class="bi bi-exclamation-triangle me-1"></i>
     Disabling this allows everyone to access and modify all resources on the server.
   </p><p class="text-danger"><i class="bi bi-exclamation-triangle me-1"></i>
-    Do not switch between WAC and ACP on an existing server
-    if its data was previously was configured with the other system.
+    If you have a running server and want to switch between authorization systems,
+    you will also have to replace the relevant authorization resources as,
+    for example, ACP can not interpret WAC authorization resources.
 `,
   options: [
     { value: 'wac', label: 'Web Access Control' },
